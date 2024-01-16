43810b5f24909b4756275b58ad6b5608a771c6ea
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-01-16 16:48:00 +0530

    Header changes

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../resourse/logo-color.png'
 
 const Header = () => {
     return (
         <>
             <div className="flex w-full justify-between py-[10px] border-t border-t-gray-300 shadow-md shadow-gray-400">
-                <div className='text-[20px] leading-[10px]'><img src={'/logo.png'} /></div>
+                <div className='text-[20px] leading-[10px]  w-44'><img src={logo} /></div>
                 <div className='flex justify-between gap-x-[20px]'>
                     <Link to="/new" className='text-[20px] leading-[10px]'>New Report</Link>
                     <Link to="/assign" className='text-[20px] leading-[10px]'>Assigned Tasks</Link>
