a076fb54c27813c4eb2802a8da209621181a386b
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-01-17 15:48:27 +0530

    All console error solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,64 +40,64 @@ const Home = () => {
       }}
     >
       <section>
-        <div class="items-center px-8 py-12 mx-auto max-w-7xl lg:px-16 md:px-12 lg:py-24">
-          <div class="justify-center w-full text-center lg:p-10 max-auto">
-            <div class="justify-center w-full mx-auto">
-              <p class="mt-8 text-5xl font-medium tracking-tighter text-black">
+        <div className="items-center px-8 py-12 mx-auto max-w-7xl lg:px-16 md:px-12 lg:py-24">
+          <div className="justify-center w-full text-center lg:p-10 max-auto">
+            <div className="justify-center w-full mx-auto">
+              <p className="mt-8 text-5xl font-medium tracking-tighter text-black">
                 TDP Reports
               </p>
-              <p class="max-w-xl mx-auto mt-4 text-lg tracking-tight text-gray-600">
+              <p className="max-w-xl mx-auto mt-4 text-lg tracking-tight text-gray-600">
               "Your Projects, Our Passion - TDP Excellence."
               </p>
             </div>
-            <div class="flex flex-col items-center justify-center max-w-xl gap-3 mx-auto mt-10 lg:flex-row">
+            <div className="flex flex-col items-center justify-center max-w-xl gap-3 mx-auto mt-10 lg:flex-row">
               <a
                 href="#_"
-                class="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full inline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none lg:w-auto focus-visible:outline-black text-sm focus-visible:ring-black"
+                className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full inline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none lg:w-auto focus-visible:outline-black text-sm focus-visible:ring-black"
               >
                 Let's Start
               </a>
               <a
                 href="#_"
-                class="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600"
+                className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600"
               >
                 Learn more &nbsp; →
               </a>
             </div>
           </div>
 
-          <div class="flex justify-center gap-6 mt-6">
-            <a class="p-1 -m-1 group" aria-label="Follow on Twitter" href="#_">
+          <div className="flex justify-center gap-6 mt-6">
+            <a className="p-1 -m-1 group" aria-label="Follow on Twitter" href="#_">
               <ion-icon
-                class="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
+                className="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
                 name="logo-twitter"
                 role="img"
                 aria-label="logo twitter"
               ></ion-icon>
             </a>
             <a
-              class="p-1 -m-1 group"
+              className="p-1 -m-1 group"
               aria-label="Follow on Instagram"
               href="#_"
             >
               <ion-icon
-                class="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
+                className="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
                 name="logo-instagram"
                 role="img"
                 aria-label="logo instagram"
               ></ion-icon>
             </a>
-            <a class="p-1 -m-1 group" aria-label="Follow on GitHub" href="#_">
+            <a className="p-1 -m-1 group" aria-label="Follow on GitHub" href="#_">
               <ion-icon
-                class="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
+                className="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
                 name="logo-github"
                 role="img"
                 aria-label="logo github"
               ></ion-icon>
             </a>
-            <a class="p-1 -m-1 group" aria-label="Follow on LinkedIn" href="#_">
+            <a className="p-1 -m-1 group" aria-label="Follow on LinkedIn" href="#_">
               <ion-icon
-                class="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
+                className="w-6 h-6 transition fill-black hover:text-blue-500 md hydrated"
                 name="logo-linkedin"
                 role="img"
                 aria-label="logo linkedin"
